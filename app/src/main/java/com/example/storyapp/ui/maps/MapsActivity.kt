d692196620b6ef4fbfbb7a8cc5952986747a8121
Anjar Harimurti <acool90@yahoo.co.id>
2023-09-11 04:26:19 +0700

    feat(maps): Add dark mode feature

--- app/src/main/java/com/example/storyapp/ui/maps/MapsActivity.kt
+++ app/src/main/java/com/example/storyapp/ui/maps/MapsActivity.kt
@@ -2,8 +2,8 @@ package com.example.storyapp.ui.maps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.example.storyapp.R
-
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -11,11 +11,16 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.example.storyapp.databinding.ActivityMapsBinding
+import com.google.android.gms.maps.model.MapStyleOptions
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+
+    private val viewModel: MapsViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,8 +28,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         setContentView(binding.root)
 
         setSupportActionBar(binding.topAppBar)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true);
-        supportActionBar?.setDisplayShowHomeEnabled(true);
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
 
         binding.topAppBar.setNavigationOnClickListener {
             finish()
@@ -37,11 +42,26 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        checkTheme()
 
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    private fun checkTheme() {
+        viewModel.getThemeSettings().observe(this) { isDarkModeActive: Boolean ->
+            if (isDarkModeActive) {
+                mMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                        this, R.raw.map_in_night
+                    )
+                )
+            } else {
+                mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+            }
+        }
     }
 
 }
---
+++ app/src/main/java/com/example/storyapp/ui/maps/MapsViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.storyapp.ui.maps
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.example.storyapp.domain.repository.StoryRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MapsViewModel @Inject constructor(
+    private val repository: StoryRepository
+) : ViewModel() {
+
+    fun getThemeSettings(): LiveData<Boolean> {
+        return repository.getThemeSettings()
+    }
+}
